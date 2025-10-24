ee36f5bc882f696a576b58a13cf40be4afff0243
JulianaPelozo <julianapelozopacheco@gmail.com>
2025-10-24 20:28:00 -0300

    Update OcorrenciaModels.ts

--- src/models/OcorrenciaModels.ts
+++ src/models/OcorrenciaModels.ts
@@ -1,54 +1,37 @@
-export class OcorrenciaModels {
+import { } from "module";
+import { Schema, model } from "mongoose";
 
-    private id: number;
-    private titulo: string;
-    private descricao: string
-    private data: Date;
-    private status: string;
-
-    constructor(id: number, titulo: string, descricao: string, data: Date, status: string) {
-        this.id = id;
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.data = data;
-        this.status = status;
-    }
-    public getId(): number {
-        return this.id;
-    }
-
-    public getTitulo(): string {
-        return this.titulo;
-    }
-
-    public getDescricao(): string {
-        return this.descricao;
-    }
-
-    public getData(): Date {
-        return this.data;
-    }
-    public getStatus(): string {
-        return this.status;
-    }
-
-    public setStatus(status: string): void {
-        this.status = status;
-    }   
-
-    public setTitulo(titulo: string): void {
-        this.titulo = titulo;
-    }   
-
-    public setDescricao(descricao: string): void {
-        this.descricao = descricao;
-    }
-
-    public setData(data: Date): void {
-        this.data = data;
-    }
-
-    public setId(id: number): void {
-        this.id = id;
-    }
+export interface IOcorrencia {
+    id: number;
+    titulo: string; 
+    descricao: string;
+    data: Date;
+    status: string;
 }
+
+class OcorrenciaModels {
+    id: number
+    titulo: string
+    descricao: string
+    data: Date
+    status: string
+    constructor(id: number, titulo: string, descricao: string, data: Date, status: string) {
+        this.id = id
+        this.titulo = titulo
+        this.descricao = descricao
+        this.data = data
+        this.status = status
+    }
+    getTitulo() {return this.titulo}
+    setTitulo(t: string) {this.titulo = t}
+}
+
+const ocorrenciaSchema = new Schema<IOcorrencia>({
+    id: { type: Number, required: true, unique: true },
+    titulo: { type: String, required: true },
+    descricao: { type: String, required: true },
+    data: { type: Date, required: true },
+    status: { type: String, required: true }
+}, { timestamps: true });
+const Ocorrencia = model<IOcorrencia>("Ocorrencia", ocorrenciaSchema);
+export { Ocorrencia, OcorrenciaModels };
